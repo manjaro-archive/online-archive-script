5c44d9e4b9c4ece7882e052ccdf64e780d870400
Stefano Capitani <stefano@manjaro.org>
2023-05-28 18:22:21 +0200

    Optimize script
    Signed-off-by: Stefano Capitani <stefano@manjaro.org>

--- grab-branchs-pkgs.sh
+++ grab-branchs-pkgs.sh
@@ -1,32 +1,42 @@
-#!/bin/bash
+#!/bin/env bash
 
 set -e
 
 source /home/tuxboyeu/domains/manjaro.tuxboy.eu/public_html/.server_url
 
-#Grab pkgs list from Manjaro repository
+# Grab pkgs list from Manjaro repository
 cd /home/tuxboyeu/domains/manjaro.tuxboy.eu/public_html/$1-archive
 startdir=$(pwd)
 
 url=$(cat $startdir/$1-list)
+pkg=$(cat $startdir/$1-list | cut -d '/' -f5)
 rm -f $startdir/.pkgs-list
 rm -f $startdir/.files-name.list
 
+# Create pkg's url
 for line in $url; do
 	echo "$server_url"/"$line" >> $startdir/.pkgs-list
     echo "$server_url"/"$line".sig >> $startdir/.pkgs-list
 done
 
-cat $startdir/.pkgs-list | xargs -n 1 -P 8 wget -nc -P $startdir
+# Check if already pkg exist and then remove from pkgs list
+for i in $pkg; do
+    if [[ -f "$startdir/$i" ]]; then
+        sed -i "/$i/d" "$startdir/.pkgs-list"
+    fi
+done
+
+# Download only the necessary pkgs
+[[ -s "$startdir/.pkgs-list" ]] && cat $startdir/.pkgs-list | xargs -n 1 -P 8 wget -nc -P $startdir
 
 echo "All files are upload into $1 server"
 
 # Create file wich containing unique filenames
 ls | sed -e 's/-\([0-9]\)/ \1/' | awk '{print $1}' | uniq >> $startdir/.files-name.list
 
-# Keep only the last 15 old versions of every file
+# Keep only the last 10 old versions of every file
 for name in $( cat $startdir/.files-name.list ); do 
-    ls -t | grep -E "^$name-+[0-9]" | awk 'NR>15 {print $1}' | xargs rm -vf
+    ls -t | grep -E "^$name-+[0-9]" | awk 'NR>10 {print $1}' | xargs rm -vf
 done
 
 echo 'Removed old files completed'
